5b24d5bf19e2764ee2aca54e4775656ea389f78a
Kamil Rybacki <krybacki@task.gda.pl>
2024-01-16 10:37:14 +0100

    save

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 pub mod problem_1;
 #[path="easy/9.palindrome-number.rs"]
 pub mod problem_9;
+#[path="easy/13.roman-to-integer.rs"]
+pub mod problem_13;
 
 fn main() {
     println!("Hello, world!");
