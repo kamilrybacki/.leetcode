24be66a5ce0520aae5e1cc21b1096e84dc8d1272
Kamil Rybacki <krybacki@task.gda.pl>
2024-01-17 16:55:53 +0100

    clear

--- src/easy/21.merge-two-sorted-lists.rs
+++ src/easy/21.merge-two-sorted-lists.rs
@@ -27,34 +27,6 @@ impl Solution {
         list1: Option<Box<ListNode>>,
         list2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
-        let mut list1_head = list1.clone();
-        let mut list2_head = list2.clone();
-        let mut nodes: Vec<Option<Box<ListNode>>> = Vec::new();
-        loop {
-            match (list1_head.clone(), list2_head.clone()) {
-                (None, None) => break,
-                (None, node2) => {
-                    nodes.push(node2.clone());
-                    list2_head = node2.unwrap().next;
-                    continue;
-                }
-                (node1, None) => {
-                  nodes.push(node1.clone());
-                  list1_head = node1.unwrap().next;
-                  continue;
-                }
-                (node1, node2) => {
-                    nodes.push(node1.clone());
-                    list1_head = node1.clone().unwrap().next;
-                    if node1.clone().unwrap().val <= node2.clone().unwrap().val {
-                        nodes.push(node2.clone());
-                        list2_head = node2.unwrap().next;
-                    }
-                    continue;
-                }
-            }
-        }
-
     }
 }
 // @lc code=end
