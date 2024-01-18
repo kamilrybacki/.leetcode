83d41bdcda32c653610ef98a0982f7620f271d15
Kamil Rybacki <krybacki@task.gda.pl>
2024-01-18 16:58:48 +0100

    save

--- src/easy/21.merge-two-sorted-lists.rs
+++ src/easy/21.merge-two-sorted-lists.rs
@@ -22,11 +22,41 @@
 //   }
 // }
 
+
 impl Solution {
     pub fn merge_two_lists(
         list1: Option<Box<ListNode>>,
         list2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
+      use std::cmp::Ordering;
+      let mut list1_head = list1.clone();
+      let mut list2_head = list2.clone();
+      let mut results: Vec<Option<Box<ListNode>>> = Vec::new();
+      while list1_head != None && list2_head != None {
+        let mut new_node = Box::new(ListNode::new(0));
+        match list1_head.clone().unwrap().val.cmp(&list2_head.clone().unwrap().val) {
+          Ordering::Less | Ordering::Equal => {
+            new_node.val = list1_head.clone().unwrap().val;
+            new_node.next = Some(Box::new(ListNode::new(
+              list2_head.clone().unwrap().val
+            )));
+          }
+          Ordering::Greater => {
+            new_node.val = list2_head.clone().unwrap().val;
+            new_node.next = Some(Box::new(ListNode::new(
+              list1_head.clone().unwrap().val
+            )));
+          }
+        }
+        println!("{:?}", new_node);
+        results.push(Some(new_node));
+        list1_head = list1_head.unwrap().next;
+        list2_head = list2_head.unwrap().next;
+      }
+      results.iter().fold(results.first().unwrap(), |acc, value| {
+        acc.unwrap().next = *value;
+        acc
+      })
     }
 }
 // @lc code=end
