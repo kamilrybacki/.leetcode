ad8ef3c6117c2c2193c45cef36cb5d8445aa08df
Kamil Rybacki <krybacki@task.gda.pl>
2024-01-19 16:55:07 +0100

    save

--- src/easy/27.remove-element.rs
+++ src/easy/27.remove-element.rs
@@ -7,9 +7,11 @@
 // @lc code=start
 impl Solution {
     pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
-        nums.iter().fold(nums.len() as i32, |mut acc, x| {
-            if *x == val {
+        nums.clone().iter().enumerate().fold(nums.len() as i32, |mut acc, (index, number)| {
+            if *number == val {
                 acc -= 1;
+                nums.push(*number);
+                nums.remove(index);
             }
             acc
         })
