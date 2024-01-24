0ba1a316ebb2e2fe7eced681f23961865f042144
Kamil Rybacki <krybacki@task.gda.pl>
2024-01-24 12:01:32 +0100

    done 70

--- src/easy/70.climbing-stairs.rs
+++ src/easy/70.climbing-stairs.rs
@@ -7,11 +7,27 @@
 // @lc code=start
 impl Solution {
     pub fn climb_stairs(n: i32) -> i32 {
-      fn calculate_combinations(steps: i32) -> i32 {
-        steps
+      use std::collections::HashMap;
+      let mut memo: HashMap<i32, i32> = HashMap::new();
+      fn calculate_combinations(steps: i32, memo: &mut HashMap<i32, i32>) -> i32 {
+        match memo.get(&steps) {
+          Some(entry) => *entry,
+          None => {
+            let result = match steps {
+              x => match x < 0 {
+                true => 0,
+                false => match x > 2 {
+                  true => calculate_combinations(steps-1, memo) + calculate_combinations(steps-2, memo),
+                  false => x
+                }
+              }
+            };
+            memo.insert(steps, result);
+            result
+          }
+        }
       }
-      return calculate_combinations(n)
+      calculate_combinations(n, &mut memo)
     }
 }
 // @lc code=end
-
