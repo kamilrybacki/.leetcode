e9f81297be3aa9e5b4080c66e99958f37dbb97b0
Kamil Rybacki <krybacki@task.gda.pl>
2024-01-29 13:53:16 +0100

    104 done

--- src/easy/104.maximum-depth-of-binary-tree.rs
+++ src/easy/104.maximum-depth-of-binary-tree.rs
@@ -26,6 +26,25 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
-    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {}
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        fn branch_depth(node: &Option<Rc<RefCell<TreeNode>>>, current_level: i32) -> Vec<i32> {
+            let mut depths = Vec::new();
+            match node {
+                Some(present_node) => {
+                    depths.append(&mut branch_depth(
+                        &present_node.borrow().left,
+                        current_level + 1,
+                    ));
+                    depths.append(&mut branch_depth(
+                        &present_node.borrow().right,
+                        current_level + 1,
+                    ));
+                }
+                None => depths.push(current_level),
+            }
+            depths
+        }
+        branch_depth(&root, 0).into_iter().max().unwrap()
+    }
 }
 // @lc code=end
