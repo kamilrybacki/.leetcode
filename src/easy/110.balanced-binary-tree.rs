7bcd26f519b76091ee1ac0cf652aebf9a34bf746
Kamil Rybacki <krybacki@task.gda.pl>
2024-01-31 16:29:53 +0100

    save

--- src/easy/110.balanced-binary-tree.rs
+++ src/easy/110.balanced-binary-tree.rs
@@ -27,7 +27,17 @@ use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn is_balanced(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
-        
+        fn measure_branch(node: &Option<Rc<RefCell<TreeNode>>>, last_height: i32) -> i32 {
+          match node {
+            None => last_height,
+            Some(existing_node) =>  {
+              let left_height = measure_branch(&existing_node.borrow().left, last_height + 1);
+              let right_height = measure_branch(&existing_node.borrow().right, last_height + 1);
+              left_height - right_height
+            }
+          }
+        }
+        measure_branch(&root, 0) == 0
     }
 }
 // @lc code=end
