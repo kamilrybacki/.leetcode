681c53000d8d29ad1eb61e9320cd8c920b48262b
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2024-02-04 14:26:06 +0100

    110 done

--- src/easy/110.balanced-binary-tree.rs
+++ src/easy/110.balanced-binary-tree.rs
@@ -27,50 +27,26 @@ use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
     pub fn is_balanced(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
-        use std::collections::HashMap;
-        fn measure_branch(node: &Option<Rc<RefCell<TreeNode>>>, last_height: i32) -> Vec<i32> {
+        fn measure_branch(node: Option<Rc<RefCell<TreeNode>>>) -> i32 {
             match node {
-                Some(parent_node) => {
-                    match (
-                        node.as_ref().unwrap().borrow().left.clone(),
-                        node.as_ref().unwrap().borrow().right.clone(),
-                    ) {
-                        (None, None) => vec![last_height, last_height],
-                        (Some(left), None) => {
-                            let mut left_height = measure_branch(&Some(left), last_height + 1);
-                            left_height.push(last_height);
-                            left_height
-                        }
-                        (None, Some(right)) => {
-                            let mut right_height = measure_branch(&Some(right), last_height + 1);
-                            right_height.insert(0, last_height);
-                            right_height
-                        }
-                        (Some(left), Some(right)) => {
-                            let mut left_height = measure_branch(&Some(left), last_height + 1);
-                            let right_height = measure_branch(&Some(right), last_height + 1);
-                            left_height.extend(right_height);
-                            left_height
-                        }
-                    }
+                None => 0,
+                Some(existing_node) => {
+                    1 + measure_branch(existing_node.borrow().left.clone())
+                        .max(measure_branch(existing_node.borrow().right.clone()))
                 }
-                None => vec![last_height, last_height],
             }
         }
-        let counts = measure_branch(&root, 0)
-            .iter()
-            .fold(HashMap::new(), |mut counters, value| {
-                counters.insert(
-                    *value,
-                    match counters.get_key_value(value) {
-                        Some((_, current_counter)) => current_counter + 1,
-                        None => 1,
-                    },
-                );
-                counters
-            });
-        counts.values().fold(0, |sum, count| (sum + count) as i32) / counts.len() as i32
-            == *counts.values().nth(0).unwrap()
+        match root {
+            None => true,
+            Some(root_node) => {
+                Self::is_balanced(root_node.borrow().left.clone())
+                    && Self::is_balanced(root_node.borrow().right.clone())
+                    && (measure_branch(root_node.borrow().left.clone())
+                        - measure_branch(root_node.borrow().right.clone()))
+                    .abs()
+                        <= 1
+            }
+        }
     }
 }
 // @lc code=end
