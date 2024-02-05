b2195ac96fd665dca6b24b931d3b2977c35a73fe
Kamil Rybacki <krybacki@task.gda.pl>
2024-02-05 14:27:08 +0100

    save

--- src/easy/111.minimum-depth-of-binary-tree.rs
+++ src/easy/111.minimum-depth-of-binary-tree.rs
@@ -4,6 +4,7 @@
  * [111] Minimum Depth of Binary Tree
  */
 
+use core::borrow;
 use std::borrow::Borrow;
 // @lc code=start
 // Definition for a binary tree node.
@@ -24,29 +25,36 @@ use std::borrow::Borrow;
 //     }
 //   }
 // }
-use std::cell::{Ref, RefCell};
+use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
     pub fn min_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        fn measure_branch(node: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-            match node {
-                None => 0,
-                Some(existing_node) => {
-                    1 + measure_branch(existing_node.borrow().left.clone())
-                        .max(measure_branch(existing_node.borrow().right.clone()))
+        fn branch_min_depth(branch: Option<Rc<RefCell<TreeNode>>>, shift: i32) -> i32 {
+            println!("{:?}: {}", branch, shift);
+            let mut minimal_depth = shift.clone();
+            match branch {
+                None => minimal_depth - 1,
+                Some(node) => {
+                    let left_depth = branch_min_depth(node.borrow().left.clone(), shift + 1);
+                    let right_depth = branch_min_depth(node.borrow().right.clone(), shift + 1);
+                    left_depth.min(right_depth)
                 }
             }
         }
         match root {
             None => 0,
             Some(valid_root) => {
-                1 + match (
-                    measure_branch(valid_root.borrow().left.clone()),
-                    measure_branch(valid_root.borrow().right.clone()),
+                let minimum_depth = match (
+                    valid_root.borrow().left.clone(),
+                    valid_root.borrow().right.clone(),
                 ) {
-                    (0, h) | (h, 0) => h,
-                    (h_left, h_right) => h_left.min(h_right),
-                }
+                    (Some(_), Some(_)) => branch_min_depth(Some(valid_root.clone()), 1),
+                    (None, None) => 1,
+                    (Some(single_branch), None) | (None, Some(single_branch)) => {
+                        branch_min_depth(Some(single_branch.clone()), 1)
+                    }
+                };
+                minimum_depth
             }
         }
     }
